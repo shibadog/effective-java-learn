4e4c01c49749375be4e43ac5ae8d11c01c099c46
shiba_dog <mischievous.dog00@gmail.com>
2019-01-08 23:57:36 +0900

    to optional

--- src/main/java/jp/shibadog/learn/effective/learn55/ToOptional.java
+++ src/main/java/jp/shibadog/learn/effective/learn55/ToOptional.java
@@ -2,17 +2,18 @@ package jp.shibadog.learn.effective.learn55;
 
 import java.util.Collection;
 import java.util.Objects;
+import java.util.Optional;
 
 public class ToOptional {
 
-    public <E extends Comparable<E>> E max(Collection<E> c) {
+    public <E extends Comparable<E>> Optional<E> max(Collection<E> c) {
         if (c.isEmpty())
-            throw new IllegalArgumentException("Empty collection");
+            return Optional.empty();
         
             E result = null;
             for (E e : c)
                 if (result == null || e.compareTo(result) > 0)
                     result = Objects.requireNonNull(e);
-            return result;
+            return Optional.of(result);
     }
 }

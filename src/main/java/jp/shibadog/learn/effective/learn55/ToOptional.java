849c4b76ffdf49840de82665431a26962adf9fb3
shiba_dog <mischievous.dog00@gmail.com>
2019-01-09 00:04:12 +0900

    stream

--- src/main/java/jp/shibadog/learn/effective/learn55/ToOptional.java
+++ src/main/java/jp/shibadog/learn/effective/learn55/ToOptional.java
@@ -1,19 +1,12 @@
 package jp.shibadog.learn.effective.learn55;
 
 import java.util.Collection;
-import java.util.Objects;
+import java.util.Comparator;
 import java.util.Optional;
 
 public class ToOptional {
 
     public <E extends Comparable<E>> Optional<E> max(Collection<E> c) {
-        if (c.isEmpty())
-            return Optional.empty();
-        
-            E result = null;
-            for (E e : c)
-                if (result == null || e.compareTo(result) > 0)
-                    result = Objects.requireNonNull(e);
-            return Optional.of(result);
+        return c.stream().max(Comparator.naturalOrder());
     }
 }
